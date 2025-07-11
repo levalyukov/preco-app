a500d331ed3411ae0947678382723227cef5e7b7
levalyukov <lev.alyukov@gmail.com>
2025-07-11 20:22:07 +0500

    fix & remove unnecessary buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,14 @@ const Header: React.FC = () => {
     const handleOpenMenu = () => {setIsMenuOpen(true);};
     const handleCloseMenu = () => {setIsMenuOpen(false);};
 
+    useEffect(() => {
+        if (isMenuOpen) {
+            const originalOverflow = document.body.style.overflow;
+            document.body.style.overflow = 'hidden';
+            return () => {document.body.style.overflow = originalOverflow;}
+        }
+    })
+
     return (
         <div id='nav-container'>
             <div 
@@ -22,23 +30,15 @@ const Header: React.FC = () => {
                 className={isMenuOpen ? 'visible' : 'invisible'}
             >
                 <div ref={mobileMenuRef} id="mobile-menu">
-                <nav>
-                    <div id='mobile-menu-header'>
-                        <p id="user"><span><FontAwesomeIcon icon={faCircleUser} /></span> Лев А. В.</p>
-                        <button id='close-mobile-nav' onClick={handleCloseMenu}><FontAwesomeIcon icon={faBars}/></button>
-                    </div>
+                    <nav>
+                        <div id='mobile-menu-header'>
+                            <p id="user"><span><FontAwesomeIcon icon={faCircleUser} /></span> Лев А. В.</p>
+                            <button id='close-mobile-nav' onClick={handleCloseMenu}><FontAwesomeIcon icon={faBars}/></button>
+                        </div>
 
-                    <a href='#'>
-                        <span><FontAwesomeIcon icon={faCalendarDays}/></span> Расписание
-                    </a>
-
-                    <a href='#'>
-                        <span><FontAwesomeIcon icon={faCalendarDays}/></span> Эл. Журнал
-                    </a>
-
-                    <a href='#'>
-                        <span><FontAwesomeIcon icon={faCalendarDays}/></span> Зачетка
-                    </a>
+                        <a href='#'>
+                            <span><FontAwesomeIcon icon={faCalendarDays}/></span> Расписание
+                        </a>
                     </nav>
                 </div>
             </div>
@@ -47,8 +47,6 @@ const Header: React.FC = () => {
                 <nav>
                     <button id='open-mobile-nav' onClick={handleOpenMenu}><span><FontAwesomeIcon icon={faBars}/></span></button>
                     <a href='#'>Расписание</a>
-                    <a href='#'>Эл. Журнал</a>
-                    <a href='#'>Зачетка</a>
                 </nav>
             </header>
         </div>
