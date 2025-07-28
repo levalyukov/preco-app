36bee9e20636bed01329382f3773d0b429365384
levalyukov <lev.alyukov@gmail.com>
2025-07-28 12:32:56 +0500

    add new routing on server

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,7 @@
 import cors from 'cors'
 import express from 'express'
-import { chromium } from 'playwright'
+import { chromium, errors } from 'playwright'
+import { Extension } from 'typescript'
 
 const app = express()
 const port = 3000
@@ -13,67 +14,98 @@ app.get('/schedule', async (req, res) => {
   let result = {}
   let index = 0
 
-  browser = await chromium.launch()
-  const page = await browser.newPage()
-  
-  await page.goto("https://moodle.preco.ru/blocks/sheduleonlineurk/sheduleonlinefree.php?datebegin[day]=13&datebegin[month]=1&datebegin[year]=2025&datefinish[day]=18&datefinish[month]=1&datefinish[year]=2025")
-  await page.waitForSelector(".select2-selection__rendered")
-  await page.click(".select2-selection__rendered")
-  await page.waitForSelector(".select2-search__field")
-  await page.fill(".select2-search__field", '110')
-  await page.click(".select2-results__option")
-  await page.click("#id_submitbutton")
-  await page.waitForSelector(".urk_shedule")
+  try {
+    browser = await chromium.launch()
+    const page = await browser.newPage()
+    
+    await page.goto(`https://moodle.preco.ru/blocks/sheduleonlineurk/sheduleonlinefree.php?datebegin[day]=13&datebegin[month]=1&datebegin[year]=2025&datefinish[day]=18&datefinish[month]=1&datefinish[year]=2025`)
+    await page.waitForSelector(".select2-selection__rendered")
+    await page.click(".select2-selection__rendered")
+    await page.waitForSelector(".select2-search__field")
+    await page.fill(".select2-search__field", req.query.group)
+    await page.click(".select2-results__option")
+    await page.click("#id_submitbutton")
+    await page.waitForSelector(".urk_shedule", {'timeout': 1500})
 
-  let shedule = page.locator(".urk_shedule")
-  let shedule_blocks = shedule.locator(".urk_sheduleblock")
-  let all_lessons = await shedule_blocks.count()
-  let shedule_visible = await shedule.isVisible()
-  let shedule_count = await shedule.count()
+    let shedule = page.locator(".urk_shedule")
+    let shedule_blocks = shedule.locator(".urk_sheduleblock")
+    let all_lessons = await shedule_blocks.count()
+    let shedule_visible = await shedule.isVisible()
+    let shedule_count = await shedule.count()
 
-  if (shedule_count > 0 && shedule_visible) {
-    for (let lesson = 0; lesson < all_lessons; lesson++) {
-      let lesson_block = shedule_blocks.nth(lesson)
-      let lesson_date = lesson_block.locator(".urk_sheduledate")
-      let dates = await lesson_date.count()
+    if (shedule_count > 0 && shedule_visible) {
+      for (let lesson = 0; lesson < all_lessons; lesson++) {
+        let lesson_block = shedule_blocks.nth(lesson)
+        let lesson_date = lesson_block.locator(".urk_sheduledate")
+        let dates = await lesson_date.count()
 
-      for (let time = 0; time < dates; time++) {
-        let time_lesson = lesson_date.nth(time)
-        let timedate = await time_lesson.textContent()
-        let target_lessons = lesson_block.locator(".urk_lessonblock")
-        let count = await target_lessons.count()
-        
-        index = 0
-        result[timedate] = {}
-        for (let contentIdx = 0; contentIdx < count; contentIdx++) {
-          index++
-           
-          let count_description = lesson_block.locator(".urk_lessondescription")
-          let nth_description = count_description.nth(contentIdx)
-          let result_description = await nth_description.textContent()
-          let target_description = result_description.trim('\n', ' ')
+        for (let time = 0; time < dates; time++) {
+          let time_lesson = lesson_date.nth(time)
+          let timedate = await time_lesson.textContent()
+          let target_lessons = lesson_block.locator(".urk_lessonblock")
+          let count = await target_lessons.count()
 
-          let target_lesson_date = target_lessons.locator(".urk_timewindow")
-          let nth_target_lesson_date = target_lesson_date.nth(contentIdx)
-          let lesson_time_coupe = nth_target_lesson_date.locator(".urk_timewindowinfo").nth(0)
-          let lesson_time_start = nth_target_lesson_date.locator(".urk_timewindowinfo").nth(1)
-          let lesson_time_end = nth_target_lesson_date.locator(".urk_timewindowinfo").nth(2)
+          index = 0
+          result[timedate] = {}
+          for (let contentIdx = 0; contentIdx < count; contentIdx++) {
+            index++
 
-          let target_time_coupe = await lesson_time_coupe.textContent()
-          let target_time_start = await lesson_time_start.textContent()
-          let target_time_end = await lesson_time_end.textContent()
+            let count_description = lesson_block.locator(".urk_lessondescription")
+            let nth_description = count_description.nth(contentIdx)
+            let result_description = await nth_description.textContent()
+            let target_description = result_description.trim('\n', ' ')
 
-          result[timedate][index] = {
-            name: target_description,
-            coupe: target_time_coupe,
-            time_start: target_time_start,
-            time_end: target_time_end
-          };
+            let target_lesson_date = target_lessons.locator(".urk_timewindow")
+            let nth_target_lesson_date = target_lesson_date.nth(contentIdx)
+            let lesson_time_coupe = nth_target_lesson_date.locator(".urk_timewindowinfo").nth(0)
+            let lesson_time_start = nth_target_lesson_date.locator(".urk_timewindowinfo").nth(1)
+            let lesson_time_end = nth_target_lesson_date.locator(".urk_timewindowinfo").nth(2)
+
+            let target_time_coupe = await lesson_time_coupe.textContent()
+            let target_time_start = await lesson_time_start.textContent()
+            let target_time_end = await lesson_time_end.textContent()
+
+            result[timedate][index] = {
+              name: target_description,
+              coupe: target_time_coupe,
+              time_start: target_time_start,
+              time_end: target_time_end
+            };
+          }
         }
       }
     }
+  } catch (error) {
+    console.error(
+      "Parser error: ", error
+    )
   }
+
   res.json(result)
+})
+
+app.get('/groups', async (req, res) => {
+  let browser = null
+  let groups_dict = {'groups': {}}
+  let group_arr = []
+
+  browser = await chromium.launch()
+  const page = await browser.newPage()
+  await page.goto(`https://moodle.preco.ru/blocks/sheduleonlineurk/sheduleonlinefree.php?datebegin[day]=13&datebegin[month]=1&datebegin[year]=2025&datefinish[day]=18&datefinish[month]=1&datefinish[year]=2025`)
+
+  await page.waitForSelector('.select2-selection__rendered')
+  await page.click('.select2-selection__rendered')
+  await page.waitForSelector('.select2-results__options')
+  let selection = page.locator('#select2-id_listgroups-results')
+  let options = selection.locator('.select2-results__option')
+
+  for (let element of await options.all()) {
+    let content = await element.innerText()
+    group_arr.push(content)
+  }
+
+  groups_dict["groups"] = group_arr  
+  res.json(groups_dict)
 })
 
 app.listen(port, host, () => {
